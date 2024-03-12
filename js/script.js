b6c6ab1776a2f281da839e03f4e3083152c59e62
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-12 14:28:30 +0100

    [add] Prev/Next Function

--- js/script.js
+++ js/script.js
@@ -37,4 +37,18 @@ createApp({
         }
     },
 
+    methods: {
+        prevNext(isNext){
+
+            if (isNext) {
+                this.counter++
+                if (this.counter === this.images.length) this.counter = 0;
+            } else {
+                this.counter--;
+                if (this.counter < 0) this.counter = this.images.length -1;
+            }
+            console.log(this.counter);
+        }
+    },
+
 }).mount('#app')
